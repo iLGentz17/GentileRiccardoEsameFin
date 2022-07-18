d12e67243da16b58df0398b8828809f0cf1ae397
Riccardo Gentile <riccardo.gentile@stud.tecnicosuperiorekennedy.it>
2022-07-18 11:28:55 +0200

    aggiornato host per AWS

--- backend/backendApp.js
+++ backend/backendApp.js
@@ -6,7 +6,7 @@ const app = express();
 
 const pool = new Pool({
     user: "itsuser",
-    host: "database",
+    host: "gentilericcardoesamefin.c9nj1x2p6gk5.eu-west-1.rds.amazonaws.com",
     database: "itsdb",
     password: "itsuser01",
     port: "5432"
